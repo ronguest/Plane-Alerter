5583284c55cd9cd002e21c94c35ace0cb85b21f5
roadfun <ronguest@protonmail.com>
2018-04-23 14:54:26 -0500

    Initial Commit

---
+++ Plane Alerter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Plane Alerter
+//
+//  Created by Ron Guest on 4/23/18.
+//  Copyright © 2018 Ron Guest. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
